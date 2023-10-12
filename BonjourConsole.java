030303c1fa0854093fc1949d309e0799e52b51b4
ramzi <ramzi.benali@gmail.comcc>
2023-10-12 16:36:39 +0200

    second

--- BonjourConsole.java
+++ BonjourConsole.java
@@ -1,6 +1,6 @@
 public class BonjourConsole {
     
     public static void main(String[] args) {
-         System.out.print("bonjour tout le monde !"); 
+         System.out.print("bonjour tout le monde"); 
     }
 }
