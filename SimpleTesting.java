001116da855976a7c7823c9bac3ac3e1becc501a
ramzi <ramzi.benali@gmail.comcc>
2023-10-12 18:26:25 +0200

    changement de nom ET1

--- SimpleTesting.java
+++ SimpleTesting.java
@@ -1,6 +1,6 @@
 public class SimpleTesting {
 	public static void main(String[] args) {
-		Student student = new Student("ramzi7");
+		Student student = new Student("ramzi");
 		String name = student.getName();
 		System.out.println("Student name is : "+name);
 	}
