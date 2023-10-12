66e8439ef0b5af307a672fb5fadc12e001eb2508
ramzi <ramzi.benali@gmail.comcc>
2023-10-12 18:02:34 +0200

    change main

--- SimpleTesting.java
+++ SimpleTesting.java
@@ -1,6 +1,6 @@
 public class SimpleTesting {
 	public static void main(String[] args) {
-		Student student = new Student("ramzi");
+		Student student = new Student("ramzi7");
 		String name = student.getName();
 		System.out.println("Student name is : "+name);
 	}
